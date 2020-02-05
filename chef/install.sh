1977ef20367053c1f6cf492e915054a6e25bf676
Douglas F Shearer <dougal.s@gmail.com>
2020-02-05 19:50:48 +0000

    Clone the repo if doesn't exist, pull otherwise.

--- chef/install.sh
+++ chef/install.sh
@@ -13,7 +13,13 @@ gem install chef -v '13.6.0'
 echo -e "|1|Qexp97voug20AhQ+UL6ag3jfUVE=|nqzbfK/ktiMkYDXSsFb5H743QA8= ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEAq2A7hRGmdnm9tUDbO9IDSwBK6TbQa+PXYPCPy6rbTrTtw7PHkccKrpp0yVhp5HdEIcKr6pLlVDBfOLX9QUsyCOV0wzfjIJNlGEYsdlLJizHhbn2mUjvSAHQqZETYP81eFzLQNnPHt4EVVUh7VfDESU84KezmD5QlWpXLmvU31/yMf+Se8xhHTvKSCZIFImWwoG6mbUoWf9nzpIoaSjB+weqqUUmpaaasXVal72J+UX2B+2RPW3RcT0eOzQgqlJL3RKrTJvdsjE3JEAvGq3lGHSZXy28G3skua2SmVi/w4yCE6gbODqnTWlg7+wC604ydGXA8VJiS5ap43JXiUFFAaQ==\n|1|QRaGpSmor3IZr/mBUQJbl0BAhJ4=|GvF3dpCS203PVoxyqVqe+qhZyJA= ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEAq2A7hRGmdnm9tUDbO9IDSwBK6TbQa+PXYPCPy6rbTrTtw7PHkccKrpp0yVhp5HdEIcKr6pLlVDBfOLX9QUsyCOV0wzfjIJNlGEYsdlLJizHhbn2mUjvSAHQqZETYP81eFzLQNnPHt4EVVUh7VfDESU84KezmD5QlWpXLmvU31/yMf+Se8xhHTvKSCZIFImWwoG6mbUoWf9nzpIoaSjB+weqqUUmpaaasXVal72J+UX2B+2RPW3RcT0eOzQgqlJL3RKrTJvdsjE3JEAvGq3lGHSZXy28G3skua2SmVi/w4yCE6gbODqnTWlg7+wC604ydGXA8VJiS5ap43JXiUFFAaQ==" >> .ssh/known_hosts
 
 # Get the repo.
-git clone git@github.com:dougal/cpu_latency_forwarder.git /opt/cpu_latency_forwarder
+if [ ! -d /opt/cpu_latency_forwarder ]
+then
+  git clone git@github.com:dougal/cpu_latency_forwarder.git /opt/cpu_latency_forwarder
+else
+  cd /opt/cpu_latency_forwarder
+  git pull
+fi
 
 # Run chef
 cd /opt/cpu_latency_forwarder/chef
