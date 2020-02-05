658bea5a74d2d6a11339e5f17f412334b75ba09a
Douglas F Shearer <dougal.s@gmail.com>
2020-02-05 19:53:29 +0000

    Install bundler.

--- chef/install.sh
+++ chef/install.sh
@@ -8,6 +8,9 @@ apt install -y libgmp-dev ruby2.5-dev
 # NOTE: Chef fixed to version pre-licencing change.
 gem install chef -v '13.6.0'
 
+# Install bundler.
+gem install bundler
+
 # Trust any host as github.
 # NOTE: Will re-add hosts if re-run.
 echo -e "|1|Qexp97voug20AhQ+UL6ag3jfUVE=|nqzbfK/ktiMkYDXSsFb5H743QA8= ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEAq2A7hRGmdnm9tUDbO9IDSwBK6TbQa+PXYPCPy6rbTrTtw7PHkccKrpp0yVhp5HdEIcKr6pLlVDBfOLX9QUsyCOV0wzfjIJNlGEYsdlLJizHhbn2mUjvSAHQqZETYP81eFzLQNnPHt4EVVUh7VfDESU84KezmD5QlWpXLmvU31/yMf+Se8xhHTvKSCZIFImWwoG6mbUoWf9nzpIoaSjB+weqqUUmpaaasXVal72J+UX2B+2RPW3RcT0eOzQgqlJL3RKrTJvdsjE3JEAvGq3lGHSZXy28G3skua2SmVi/w4yCE6gbODqnTWlg7+wC604ydGXA8VJiS5ap43JXiUFFAaQ==\n|1|QRaGpSmor3IZr/mBUQJbl0BAhJ4=|GvF3dpCS203PVoxyqVqe+qhZyJA= ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEAq2A7hRGmdnm9tUDbO9IDSwBK6TbQa+PXYPCPy6rbTrTtw7PHkccKrpp0yVhp5HdEIcKr6pLlVDBfOLX9QUsyCOV0wzfjIJNlGEYsdlLJizHhbn2mUjvSAHQqZETYP81eFzLQNnPHt4EVVUh7VfDESU84KezmD5QlWpXLmvU31/yMf+Se8xhHTvKSCZIFImWwoG6mbUoWf9nzpIoaSjB+weqqUUmpaaasXVal72J+UX2B+2RPW3RcT0eOzQgqlJL3RKrTJvdsjE3JEAvGq3lGHSZXy28G3skua2SmVi/w4yCE6gbODqnTWlg7+wC604ydGXA8VJiS5ap43JXiUFFAaQ==" >> /root/.ssh/known_hosts
